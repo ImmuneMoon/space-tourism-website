07b55e8ea9a1c6261ee0e330e383184269355a5a
Alex <page.alexanderjohn@gmail.com>
2023-05-25 20:03:20 -0400

    Created classes to handle styling for dest nav
    It works great on a fresh load but loses all functionality when switching to another component page until the page is refreshed. Needs work

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,152 +1,157 @@
 // try to implement what you can in the componenets before 
 // using this file
 
-document.addEventListener("DOMContentLoaded", function(event) {
+// Immediately Invoked Function Expression (IIFE) creates a new scope to prevent conflicts with other scripts
+(function() {
+    // Class for destination styling
+    class DestNavStyler {
+        constructor(ctnr, bttn, ul) {
+            this.ctnr = ctnr;
+            this.bttn = bttn;
+            this.ul = ul;
+        }
     
-    const moonCtnr = document.getElementById('moon-ctnr');
-    const moonBttn = document.getElementById('moon');
-    const moonUL = document.getElementById('moon-ul');
-
-    const marsCtnr = document.getElementById('mars-ctnr');
-    const marsBttn = document.getElementById('mars');
-    const marsUL = document.getElementById('mars-ul');
-
-    const europaCtnr = document.getElementById('europa-ctnr');
-    const europaBttn = document.getElementById('europa');
-    const europaUL = document.getElementById('europa-ul');
-
-    const titanCtnr = document.getElementById('titan-ctnr');
-    const titanBttn = document.getElementById('titan');
-    const titanUL = document.getElementById('titan-ul');
-
-    // should probably make a class to style each clicked element
-
-    document.addEventListener('click', (event) => {
-        let target = event.target;
-
-        if (target === moonBttn) {
-            moonBttn.style.color = '#FFFFFF';
-            moonUL.style.borderbottom = '3px solid #FFFFFF';
-            moonCtnr.style.display = 'block';
-            marsCtnr.style.display = 'none';
-            europaCtnr.style.display = 'none';
-            titanCtnr.style.display = 'none';
+        // Method to set the active styles when the element is clicked
+        setDestActiveStyles() {
+            this.bttn.style.color = '#FFFFFF';
+            this.ul.style.display = 'block';
+            this.ul.style.borderBottom = '3px solid #FFFFFF';
+            this.ctnr.style.display = 'block';
+        }
+    
+        // Method to set the inactive styles when the element is not clicked
+        setDestInactiveStyles() {
+            this.bttn.style.color = '#D0D6F9';
+            this.ul.style.display = 'none';
+            this.ctnr.style.display = 'none';
         }
 
-        if (target === marsBttn) {
-            marsBttn.style.color = '#FFFFFF';
-            marsUL.style.borderbottom = '3p solid #FFFFFF';
-            moonCtnr.style.display = 'none';
-            marsCtnr.style.display = 'block';
-            europaCtnr.style.display = 'none';
-            titanCtnr.style.display = 'none';
+        // Method to set the hover styles when the mouse enters the element
+        setDestNavHoverStyles() {
+            if (this.ctnr.style.display !== 'block') {
+                this.bttn.style.color = '#FFFFFF';
+                this.ul.style.display = 'block';
+                this.ul.style.borderBottom = '3px solid #ffffff86';
+            }
         }
 
-        if (target === europaBttn) {
-            europaBttn.style.color = '#FFFFFF';
-            marsUL.style.borderbottom = '3px solid #FFFFFF';
-            moonCtnr.style.display = 'none';
-            marsCtnr.style.display = 'none';
-            europaCtnr.style.display = 'block';
-            titanCtnr.style.display = 'none';
+        // Method to remove the hover styles when the mouse leaves the element
+        removeDestNavHoverStyles() {
+            if (this.ctnr.style.display !== 'block') {
+                this.bttn.style.color = '#D0D6F9';
+                this.ul.style.display = 'none';
+            }
         }
+    }
 
-        if (target === titanBttn) {
-            titanBttn.style.color = '#FFFFFF';
-            titanUL.style.borderbottom = '3px solid #FFFFFF';
-            moonCtnr.style.display = 'none';
-            marsCtnr.style.display = 'none';
-            europaCtnr.style.display = 'none';
-            titanCtnr.style.display = 'block';
-        }
+    // Waits for DOM load
+    document.addEventListener("DOMContentLoaded", () => {
         
+        // Grabs destination planets , relevant buttons and button underlines
+        const moonCtnr = document.getElementById('moon-ctnr');
+        const moonBttn = document.getElementById('moon');
+        const moonUL = document.getElementById('moon-ul');
 
-        if (moonCtnr.style.display === 'block') {
-            moonBttn.style.color = '#FFFFFF';
-            moonUL.style.borderbottom = '3px solid #FFFFFF';
+        const marsCtnr = document.getElementById('mars-ctnr');
+        const marsBttn = document.getElementById('mars');
+        const marsUL = document.getElementById('mars-ul');
+
+        const europaCtnr = document.getElementById('europa-ctnr');
+        const europaBttn = document.getElementById('europa');
+        const europaUL = document.getElementById('europa-ul');
+
+        const titanCtnr = document.getElementById('titan-ctnr');
+        const titanBttn = document.getElementById('titan');
+        const titanUL = document.getElementById('titan-ul');
+
+    // Create instances for each dest nav related element
+        const moonStyler = new DestNavStyler(moonCtnr, moonBttn, moonUL);
+        const marsStyler = new DestNavStyler(marsCtnr, marsBttn, marsUL);
+        const europaStyler = new DestNavStyler(europaCtnr, europaBttn, europaUL);
+        const titanStyler = new DestNavStyler(titanCtnr, titanBttn, titanUL);
+
+        // Sets active styles for the current planet with a display of 'block' on load
+        if (moonCtnr.style.display !== 'none') {
+            moonStyler.setDestActiveStyles();
         }
-        else if (marsCtnr.style.display === 'block') {
-            marsBttn.style.color = '#FFFFFF';
-            marsUL.style.borderbottom = '3px solid #FFFFFF';
+        else if (marsCtnr.style.display !== 'none') {
+            marsStyler.setDestActiveStyles();
         }
-        else if (europaCtnr.style.display === 'block') {
-            europaBttn.style.color = '#FFFFFF';
-            europaUL.style.borderbottom = '3px solid #FFFFFF';
+        else if (europaCtnr.style.display !== 'none') {
+            europaStyler.setDestActiveStyles();
         }
-        else if (titanCtnr.style.display === 'block') {
-            titanBttn.style.color = '#FFFFFF';
-            titanUL.style.borderbottom = '3px solid #FFFFFF';
+        else if (titanCtnr.style.display !== 'none') {
+            titanStyler.setDestActiveStyles();
         }
+
+        // Event listeners
+        document.addEventListener('click', (event) => {
+            let target = event.target;
+
+            // Displays the relevant planet component when its button is clicked
+            if (target === moonBttn) {
+                moonStyler.setDestActiveStyles();
+                marsStyler.setDestInactiveStyles();
+                europaStyler.setDestInactiveStyles();
+                titanStyler.setDestInactiveStyles();
+            } 
+            else if (target === marsBttn) {
+                marsStyler.setDestActiveStyles();
+                moonStyler.setDestInactiveStyles();
+                europaStyler.setDestInactiveStyles();
+                titanStyler.setDestInactiveStyles();
+            } 
+            else if (target === europaBttn) {
+                europaStyler.setDestActiveStyles();
+                moonStyler.setDestInactiveStyles();
+                marsStyler.setDestInactiveStyles();
+                titanStyler.setDestInactiveStyles();
+            } 
+            else if (target === titanBttn) {
+                titanStyler.setDestActiveStyles();
+                moonStyler.setDestInactiveStyles();
+                marsStyler.setDestInactiveStyles();
+                europaStyler.setDestInactiveStyles();
+            }
+        });
+
+        document.addEventListener('mouseover', (event) => {
+            let target = event.target;
+
+            // For hovering over the dest nav buttons
+            if (target === moonBttn) {
+                moonStyler.setDestNavHoverStyles();
+            } 
+            else if (target === marsBttn) {
+                marsStyler.setDestNavHoverStyles();
+            } 
+            else if (target === europaBttn) {
+                europaStyler.setDestNavHoverStyles();
+            } 
+            else if (target === titanBttn) {
+                titanStyler.setDestNavHoverStyles();
+            }
+        });
+
+        document.addEventListener('mouseout', (event) => {
+            let target = event.target;
+
+            // For then no longer hovering a dest nav button
+            if (target === moonBttn) {
+                moonStyler.removeDestNavHoverStyles();
+            } 
+            else if (target === marsBttn) {
+                marsStyler.removeDestNavHoverStyles();
+            } 
+            else if (target === europaBttn) {
+                europaStyler.removeDestNavHoverStyles();
+            }
+            else if (target === titanBttn) {
+                titanStyler.removeDestNavHoverStyles();
+            }
+
+        });
 
     });
 
-    // Should probably make a couple classes that have stylings for each dest nav element on mouse entering and leaving
-
-    document.addEventListener('mouseover', (event) => {
-        let target = event.target;
-
-         if (target === moonBttn) {
-            if (moonCtnr.style.display !== 'block') {
-                moonBttn.style.color = '#FFFFFF';
-                moonUL.style.display = 'block';
-                moonUL.style.borderBottom = '3px solid #ffffff86';
-            }
-         }
-
-         if (target === marsBttn) {
-            if (marsCtnr.style.display !== 'block') {
-                marsBttn.style.color = '#FFFFFF';
-                marsUL.style.display = 'block';
-                marsUL.style.borderBottom = '3px solid #ffffff86';
-            }
-         }
-
-         if (target === europaBttn) {
-            if (europaCtnr.style.display !== 'block') {
-                europaBttn.style.color = '#FFFFFF';
-                europaUL.style.display = 'block';
-                europaUL.style.borderBottom = '3px solid #ffffff86';
-            }
-         }
-
-         if (target === titanBttn) {
-            if (titanCtnr.style.display !== 'block') {
-                titanBttn.style.color = '#FFFFFF';
-                titanUL.style.display = 'block';
-                titanUL.style.borderBottom = '3px solid #ffffff86';
-            }
-         }
-    });
-
-    document.addEventListener('mouseout', (event) => {
-        let target = event.target;
-
-         if (target === moonBttn) {
-            if (moonCtnr.style.display !== 'block') {
-                moonBttn.style.color = '#D0D6F9';
-                moonUL.style.display = 'none';
-            }
-         }
-
-         if (target === marsBttn) {
-            if (marsCtnr.style.display !== 'block') {
-                marsBttn.style.color = '#D0D6F9';
-                marsUL.style.display = 'none';
-            }
-         }
-
-         if (target === europaBttn) {
-            if (europaCtnr.style.display !== 'block') {
-                europaBttn.style.color = '#D0D6F9';
-                europaUL.style.display = 'none';
-            }
-         }
-
-         if (target === titanBttn) {
-            if (titanCtnr.style.display !== 'block') {
-                titanBttn.style.color = '#D0D6F9';
-                titanUL.style.display = 'none';
-            }
-         }
-    });
-  });
+})();
